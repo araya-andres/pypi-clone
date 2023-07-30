f36b306fda2fae3fa1926390f1af0cb90312a6fa
Andres Araya <araya.andres@gmail.com>
2023-07-30 17:56:13 +0000

    first jinja template

--- app.py
+++ app.py
@@ -3,6 +3,15 @@ import flask
 app = flask.Flask(__name__)
 
 
+def get_latest_packages():
+    return [
+        {"name": "flask", "version": "1.2.3"},
+        {"name": "sqlalchemy", "version": "2.2.0"},
+        {"name": "passlib", "version": "3.0.0"},
+    ]
+
+
 @app.route("/")
 def index():
-    return "hello, world"
+    test_packages = get_latest_packages()
+    return flask.render_template("index.html", packages=test_packages)
