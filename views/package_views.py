a44666d73d945af1b1228afe8fd3bbae0e7cb1e4
Andres Araya <araya.andres@gmail.com>
2023-08-03 02:22:04 +0000

    adding a constrained route

--- views/package_views.py
+++ views/package_views.py
@@ -1,10 +1,13 @@
 import flask
 
-from infrastructure.view_modifiers import response
-
 blueprint = flask.Blueprint("packages", __name__, template_folder="templates")
 
 
 @blueprint.route("/project/<package_name>")
 def package_details(package_name: str):
     return f"Package details for {package_name}"
+
+
+@blueprint.route("/<int:rank>")
+def popular(rank: int):
+    return f"The details for the {rank}th most package packages"
